0b1a23fbd7adbcf628b5be09330908fab88ce0cb
javeirael <106691655+javeirael@users.noreply.github.com>
2022-06-22 18:26:58 -0500

    Delete alexsm31 directory

--- alexsm31/acciones/eliminarfabricante.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('../conec.php');
-$codigoFabricante= $_GET['id'];
-$eliminarFabricante="CALL sp_eliminarFabricante('$codigoFabricante')";;
-$resultado=mysqli_query($conexion,$eliminarFabricante);
-header('Location: ../fabricantes.php');
-?>
